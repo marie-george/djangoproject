2e76b4f88d9ca818c4b35d661e70f647cc0556cd
marie-george <maria85night@gmail.com>
2023-05-30 05:47:57 +0300

    created model Product

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=150, verbose_name='наименование')
+    description = models.CharField(max_length=500, verbose_name='описание')
+    category = models.CharField(max_length=150, verbose_name='категория')
+    price = models.IntegerField(verbose_name='цена за покупку')
+    creation_date = models.DateTimeField(verbose_name='дата создания')
+    last_change_date = models.DateTimeField(verbose_name='дата последнего изменения')
+    preview = models.ImageField(upload_to='product/', verbose_name='изображение', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.name} - {self.price}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ('name',)
+
--- config/settings.py
+++ config/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'djangoproject',
-        'USER': 'postrges',
+        'USER': 'postgres',
         'PASSWORD': os.getenv('postgres_password')
     }
 }
@@ -120,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
