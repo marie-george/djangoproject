3397c1d3a79e0088968466d2da53cb5248fd4ac0
marie-george <maria85night@gmail.com>
2023-06-01 22:33:57 +0300

    corrections to fields of model Project

--- catalog/models.py
+++ catalog/models.py
@@ -9,8 +9,8 @@ class Product(models.Model):
     description = models.CharField(max_length=500, verbose_name='описание')
     category = models.CharField(max_length=150, verbose_name='категория')
     price = models.IntegerField(verbose_name='цена за покупку')
-    creation_date = models.DateTimeField(verbose_name='дата создания')
-    last_change_date = models.DateTimeField(verbose_name='дата последнего изменения')
+    creation_date = models.DateField(verbose_name='дата создания')
+    last_change_date = models.DateField(verbose_name='дата последнего изменения')
     preview = models.ImageField(upload_to='product/', verbose_name='изображение', **NULLABLE)
 
     def __str__(self):
