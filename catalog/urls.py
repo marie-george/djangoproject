e6dd65f6eed1456686f616f78ffe1e7c80d98468
marie-george <maria85night@gmail.com>
2023-06-21 00:41:55 +0300

    CRUD Read to model Blog

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from catalog.apps import CatalogConfig
-from catalog.views import IndexView, ContactsView, ProductListView, ProductDetailView
+from catalog.views import IndexView, ContactsView, ProductListView, ProductDetailView, BlogListView, BlogDetailView
 
 app_name = CatalogConfig.name
 
@@ -10,4 +10,6 @@ urlpatterns = [
     path('contacts/', ContactsView.as_view(), name='contacts'),
     path('product_list/', ProductListView.as_view(), name='product_list'),
     path('product_item/<int:pk>', ProductDetailView.as_view(), name='product_item'),
+    path('blog_list/', BlogListView.as_view(), name='blog_list'),
+    path('blog_detail/<int:pk>', BlogDetailView.as_view(), name='product_item')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
-from catalog.models import Product
+from catalog.models import Product, Blog
 
 
 class IndexView(generic.TemplateView):
@@ -11,14 +12,6 @@ class IndexView(generic.TemplateView):
         'object_list': Product.objects.all()[:4],
     }
 
-
-# def contacts(request):
-#     if request.method == 'POST':
-#         name = request.POST.get('name')
-#         phone = request.POST.get('phone')
-#         message = request.POST.get('message')
-#         print(f'User{name} with phone {phone} send message {message}')
-#     return render(request, 'catalog/contacts.html')
 
 class ContactsView(generic.TemplateView):
     template_name = 'catalog/contacts.html'
@@ -38,3 +31,17 @@ class ProductDetailView(generic.DetailView):
         context_data = super().get_context_data(**kwargs)
         context_data['title'] = self.get_object()
         return context_data
+
+
+class BlogListView(generic.ListView):
+    model = Blog
+
+
+class BlogDetailView(generic.DetailView):
+    model = Blog
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['title'] = self.get_object()
+        return context_data
+
