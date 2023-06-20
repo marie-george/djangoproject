4bcd452fc534081f4911d39440434bf03aeeb643
marie-george <maria85night@gmail.com>
2023-06-20 23:55:12 +0300

    added Blog model

--- catalog/models.py
+++ catalog/models.py
@@ -33,3 +33,21 @@ class Category(models.Model):
         verbose_name = 'категория'
         verbose_name_plural = 'категории'
         ordering = ('name',)
+
+
+class Blog(models.Model):
+    name = models.CharField(max_length=150, verbose_name='наименование')
+    slug = models.SlugField(max_length=150, unique=True, verbose_name='URl')
+    contents = models.CharField(max_length=500, verbose_name='описание')
+    creation_date = models.DateField(verbose_name='дата создания')
+    preview = models.ImageField(upload_to='blog/', verbose_name='изображение', **NULLABLE)
+    is_published = models.BooleanField(default=True, verbose_name='признак публикации')
+    views_number = models.IntegerField(default=0, verbose_name='Количество просмотров')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'блог'
+        verbose_name_plural = 'блоги'
+        ordering = ('name',)
