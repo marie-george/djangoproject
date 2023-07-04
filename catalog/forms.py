36a01fed943c560106162d28baf72109a44dc8aa
marie-george <maria85night@gmail.com>
2023-07-04 17:00:03 +0300

    added prohibited products

--- catalog/forms.py
+++ catalog/forms.py
@@ -9,3 +9,27 @@ class ProductForm(forms.ModelForm):
         model = Product
         exclude = ('slug',)
 
+    def clean_name(self):
+        cleaned_data = self.cleaned_data['name']
+
+        unacceptables = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+
+        for unit in unacceptables:
+            cleaned_data = cleaned_data.lower()
+            if unit in cleaned_data:
+                raise forms.ValidationError('В названии или описании есть слова, которые запрещены к использованию на данном сайте')
+
+        return cleaned_data
+
+    def clean_description(self):
+        cleaned_data = self.cleaned_data['description']
+
+        unacceptables = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+
+        for unit in unacceptables:
+            cleaned_data = cleaned_data.lower()
+            if unit in cleaned_data:
+                raise forms.ValidationError('В названии или описании есть слова, которые запрещены к использованию на данном сайте')
+
+        return cleaned_data
+
