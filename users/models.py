b28637967f26fe8ec2d7334c8293aec24504ba2c
marie-george <maria85night@gmail.com>
2023-07-13 02:21:27 +0300

    created models in user app

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from catalog.models import NULLABLE
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='email')
+    phone = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
+    country = models.CharField(max_length=50, verbose_name='страна')
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+
