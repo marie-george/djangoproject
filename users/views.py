33c9a01fc441ea98d03d7a0d9219981931ad5bc9
marie-george <maria85night@gmail.com>
2023-07-13 03:02:09 +0300

    added views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import UpdateView
+from users.models import User
+from users.forms import UserForm
 
-# Create your views here.
+class ProfileUpdateView(UpdateView):
+    model = User
+    form_class = UserForm
+    success_url = reverse_lazy('users:profile')
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+
